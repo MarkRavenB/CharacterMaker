770177fc55dd21c73f74c9b5d3ea58d81f22cf00
Mark Raven Baybay <116793842+MarkRavenB@users.noreply.github.com>
2022-12-11 15:40:58 +0800

    Create CharacterVarietyList.py

---
+++ CharacterVarietyList.py
@@ -0,0 +1,16 @@
+race = []
+name = []
+skills = []
+role = []
+item = []
+personality = []
+
+def menu():
+    print("\n========================")
+    print("=====Welcome User!======")
+    print("========================")
+    print("1. Create Character")
+    print("2. View Character")
+    print("3. Delete Character")
+    print("4. Update Character")
+    print("5. Exit")
